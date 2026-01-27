93b9b038931335da5bda2cd513f61e30f3336fbd
EmirAkagunduz16 <akagunduzemir04@gmail.com>
2026-01-27 21:43:30 +0300

    upd

---
+++ apps/web/src/common/components/TopPalette/TopPalette.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../../styles/components/top-palette.styles.scss"
+
+const TopPalette = () => {
+  return (
+    <div className="top-palette-container">
+      <div className="red" />
+      <div className="orange" />
+      <div className="light-green" />
+      <div className="dark-green" />
+      <div className="blue" />
+    </div>
+  );
+};
+
+export default TopPalette;
